a90060bf0984692d2b776acb516fa58aa45d9d33
Luiz Guilherme <luizguilhermefreitasneto@gmail.com>
2025-07-08 08:48:34 -0300

    feat: crud user

---
+++ src/modules/users/user.services.ts
@@ -0,0 +1,34 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { User } from 'generated/prisma';
+
+@Injectable()
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+  async create(body: any): Promise<User> {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    return await this.prisma.user.create({ data: body });
+  }
+  async list() {
+    return await this.prisma.user.findMany();
+  }
+  async show(id: string) {
+    const user = await this.prisma.user.findUnique({
+      where: { id: Number(id) },
+    });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
+  }
+  async update(id: string, body: any) {
+    return await this.prisma.user.update({
+      where: { id: Number(id) },
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      data: body,
+    });
+  }
+  async delete(id: string) {
+    return await this.prisma.user.delete({ where: { id: Number(id) } });
+  }
+}
