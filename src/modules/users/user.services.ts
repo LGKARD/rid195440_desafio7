f4d282fec47c8de961edcffd1f655471184c4b23
Luiz Guilherme <luizguilhermefreitasneto@gmail.com>
2025-07-11 07:55:25 -0300

    feat: userSelectFields

--- src/modules/users/user.services.ts
+++ src/modules/users/user.services.ts
@@ -4,16 +4,22 @@ import { User } from 'generated/prisma';
 import { CreateUserDTO } from './domain/dto/createUser.dto';
 import { UpdateUserDTO } from './domain/dto/updateUser.dto';
 import * as bcrypt from 'bcrypt';
+import { userSelectFields } from '../prisma/utils/userSelectFields';
 
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
   async create(body: CreateUserDTO): Promise<User> {
     body.password = await this.hashPassword(body.password);
-    return await this.prisma.user.create({ data: body });
+    return await this.prisma.user.create({
+      data: body,
+      select: userSelectFields,
+    });
   }
   async list() {
-    return await this.prisma.user.findMany();
+    return await this.prisma.user.findMany({
+      select: userSelectFields,
+    });
   }
   async show(id: number) {
     const user = await this.isIdExists(id);
@@ -28,6 +34,7 @@ export class UserService {
     return await this.prisma.user.update({
       where: { id },
       data: body,
+      select: userSelectFields,
     });
   }
 
@@ -39,6 +46,7 @@ export class UserService {
   private async isIdExists(id: number) {
     const user = await this.prisma.user.findUnique({
       where: { id },
+      select: userSelectFields,
     });
     if (!user) {
       throw new NotFoundException('User not found');
